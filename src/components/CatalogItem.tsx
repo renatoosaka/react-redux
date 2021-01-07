e0add97a4b68506c0e780efa3f8271f5625c8841
Renato Hideki Osaka <renato.osaka@hotmail.com>
2021-01-07 14:55:45 -0300

    Redux

---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,32 @@
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IProduct } from '../store/modules/cart/types';
+import { addProductToCartRequest } from '../store/modules/cart/actions';
+import { IState } from '../store';
+
+interface ICatalogItemProps {
+  product: IProduct;
+}
+
+const CatalogItem: React.FC<ICatalogItemProps> = ({ product }) => {
+  const dispatch = useDispatch()
+  const hasFailedStockCheck = useSelector<IState, boolean>(state => {
+    return state.cart.failedStockCheck.includes(product.id)
+  })
+
+  const handleAddProductToCart = useCallback(() => {
+    dispatch(addProductToCartRequest(product))
+  }, [dispatch, product])
+
+
+  return (
+      <article>
+        <strong>{product.title}</strong> - <span>{product.price}</span> | 
+        <button type="button" onClick={handleAddProductToCart}>comprar</button>
+
+        {hasFailedStockCheck && <span style={{ color: 'red'}}>Acabou estoque</span>}
+      </article>    
+  );
+}
+
+export default CatalogItem;
---
+++ src/store/modules/cart/actions.ts
@@ -0,0 +1,27 @@
+import { ActionTypes, IProduct } from "./types";
+
+export function addProductToCartRequest(product: IProduct) {
+  return {
+    type: ActionTypes.addProductToCartRequest,
+    payload: {
+      product
+    }
+  }
+}
+export function addProductToCartSuccess(product: IProduct) {
+  return {
+    type: ActionTypes.addProductToCartSuccess,
+    payload: {
+      product
+    }
+  }
+}
+
+export function addProductToCartFailure(productID: number) {
+  return {
+    type: ActionTypes.addProductToCartFailure,
+    payload: {
+      productID
+    }
+  }
+}
